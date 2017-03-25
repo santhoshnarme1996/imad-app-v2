4414b8c0c17f4c18897c3464b3fd7035e739431e
Santhosh Narayanan <santhoshnarme@gmail.com>
2017-03-25 19:39:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-three', function (req,res) {
 });
 
 app.get('/article-one', function (req,res) {
-    res.send('Article one is requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
